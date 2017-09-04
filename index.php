78f34753a6ed129791c73c134a28fdbd050e69d0
Bruna <bcr.furb@gmail.com>
2017-09-04 16:22:52 -0300

    Changed css rules

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
     <!---->
 	<link rel="stylesheet" href="css/style.css">
 
-	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	
+	<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -33,34 +33,59 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 col-sm-3" style="text-align: center;width: auto;">
-					<h1 id="titulo" style="background-color: white;color:#000;font-size: 80pt;">
-<strong>PROJETO&nbsp;ACREDITO</strong></h1>
+					<h1 id="titulo">PRO<wbr>JE<wbr>TO&nbsp;A<wbr>CRE<wbr>DI<wbr>TO</h1>
 					<h3><small class="subtitulo pull-right">Ciência da Computação - FURB</small></h3>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<div><?php include 'menu.php'; ?></div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<ul class="nav nav-pills categories" style="font-weight:bold;">
+				<li>
+					<a href="index.php">INÍCIO</a>
+				</li>
+				<li>
+					<a href="atas.php">ATA DAS REUNIÕES</a>
+				</li>
+				<li>
+					<a href="projetos.php">PROJETOS ENVIADOS</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="formulario.php">FORMULÁRIO DE ENVIO</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="documentos.php">DOCUMENTOS </a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="transparencia.php">TRANSPARÊNCIA</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="sobre.php">QUEM SOMOS</a>
+				</li>
+			</ul>
+		</div>
+	</nav>	
 
 	<div class="container post">
 		<div class="row">
 			<div class="col-md-12 col-md-offset-0 post-body">
-				<h4>O que é o Projeto Acredito?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;O que é o Projeto Acredito?</h4>
 				<p>É um programa de captação de recursos financeiros junto aos alunos do curso Bacharel em Ciência da Computação da Fundação Universidade
 					Regional de Blumenau (FURB), visando a atualização tecnológica da infraestrutura do curso (equipamentos e softwares),
 					além de viabilizar o auxílio a eventos que permitam também a atualização científica dos nossos alunos.</p>
 				<hr>
-				<h4>Como Funciona?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;Como Funciona?</h4>
 				<p>Cada aluno matriculado no curso de Ciência da Computação paga mensalmente 1 (um) crédito financeiro. Esse dinheiro é
 					mantido pela FURB, porém, sua aplicação fica a disposição de projetos encaminhados pelos alunos, descentralizando desta
 					forma sua aplicação, que será feita de acordo com a demanda apresentada.</p>
 				<hr>
-				<h4>...E se o Projeto Acredito não existisse?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;...E se o Projeto Acredito não existisse?</h4>
 				<p>Os alunos teriam que pagar a quantia de créditos financeiros vinculados ao curso para a utilização dos laboratórios,
 					além de eventos como Semanas Acadêmica e de Extensão.</p>
 				<hr>
-				<h4>Qual a vantagem do Projeto Acredito em relação ao Crédito Financeiro?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;Qual a vantagem do Projeto Acredito em relação ao Crédito Financeiro?</h4>
 				<p>O dinheiro arrecadado pelo Crédito Financeiro é gerenciado exclusivamente pela FURB e está fortemente vinculado com a
 					manutenção da instituição, ou seja, é aplicado de forma geral e abrangente. Já o dinheiro arrecadado com o Projeto Acredito,
 					é gerenciado pelo Conselho Deliberativo do Projeto Acredito juntamente com a Pró-Reitoria de Administração - PROAD,
@@ -68,7 +93,7 @@
 					os alunos, que podem contar ao longo do curso com uma infraestrutura sempre atualizada no que se refere ao parque tecnológico
 					como também eventuais outras demandas que venham a ser apresentadas pelos alunos no decorrer da graduação.</p>
 				<hr>
-				<h4>O que já foi feito com o dinheiro arrecadado?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;O que já foi feito com o dinheiro arrecadado?</h4>
 				<p>Entre 2010 e 2011, o Projeto Acredito, junto com os recursos do Laboratório de Qualidade de Software (LQS), custeou três
 					laboratórios inteiros, dois de 15 máquinas (campus 3) e um de 30 máquinas (campus 1 - S-212). Destes, todos os monitores
 					foram comprados com o dinheiro do Projeto Acredito, já os gabinetes, teclados e mouses foram custeados pelo Laboratório
@@ -78,11 +103,11 @@
 					e SEMINCO (coffe break estadias dos palestrantes). Ainda foi utilizado para levar os alunos da Computação ao FLISOL
 					em Joinville.</p>
 				<hr>
-				<h4>Quem pode utilizar o dinheiro arrecadado?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;Quem pode utilizar o dinheiro arrecadado?</h4>
 				<p>O dinheiro pode ser utilizado por projetos que tenham sido aprovados pelo conselho Deliberativo do Projeto Acredito e
 					autorizados pela PROAD.</p>
 				<hr>
-				<h4>Por que o Acredito não financia eventos como Stamm Universitário?</h4>
+				<h4><i class="fa fa-chevron-circle-right"></i>&nbsp;Por que o Acredito não financia eventos como Stamm Universitário?</h4>
 				<p>Além de infringir o estatuto do Projeto Acredito, o dinheiro arrecadado é destinado a <strong>projetos que agreguem</strong> algum conhecimento
 					ou <strong>tragam algum benefício a um maior número de estudantes</strong> do curso. Embora entendamos a importância de eventos desta
 					natureza, que promovem uma maior integração entre a comunidade universitária, a temática foge da proposta de criação
@@ -91,13 +116,13 @@
 					estaríamos incorrendo em uma ilegalidade. Soma-se, ainda, o fato de a Universidade ser pública e como tal deve prestar
 					contas de toda e qualquer despesa, inclusive dos recursos do Acredito.</p>
 				<hr>
-			
+
 			</div>
 		</div>
 	</div>
-	
+
 	<footer style="background-color:	#325223;">
-		<h5>Projeto Acredito&nbsp;© 2017</h5>
+		<h5>Projeto Acredito&nbsp;© 2017 | Estamos no <a target="_blank" class="git" alt="GitHub" href="https://www.github.com/calcomp/projeto-acredito">GitHub</a>.</h5>
 	</footer>
 
 	<a href="javascript:" id="return-top"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
--- menu.php
+++ menu.php
@@ -1,24 +1,34 @@
-<ul class="nav nav-pills categories" style="font-weight:bold;">
-		<li>
-			<a href="index.php">INÍCIO</a>
-		</li>
-		<li>
-			<a href="atas.php">ATA DAS REUNIÕES</a>
-		</li>
-		<li>
-			<a href="projetos.php">PROJETOS ENVIADOS</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="formulario.php">FORMULÁRIO DE ENVIO</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="documentos.php">DOCUMENTOS </a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="transparencia.php">TRANSPARÊNCIA</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="sobre.php">QUEM SOMOS</a>
-		</li>
-	</ul>
-</div>
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+		<ul class="nav navbar-nav categories" style="font-weight:bold;">
+			<li>
+				<a style="color:white !important;" href="index.php">INÍCIO</a>
+			</li>
+			<li>
+				<a style="color:white !important;" href="atas.php">ATA DAS REUNIÕES</a>
+			</li>
+			<li>
+				<a style="color:white !important;" href="projetos.php">PROJETOS ENVIADOS</a>
+			</li>
+			<li class="nav-item">
+				<a style="color:white !important;" class="nav-link" href="formulario.php">FORMULÁRIO DE ENVIO</a>
+			</li>
+			<li class="nav-item">
+				<a style="color:white !important;" class="nav-link" href="documentos.php">DOCUMENTOS </a>
+			</li>
+			<li class="nav-item">
+				<a style="color:white !important;" class="nav-link" href="transparencia.php">TRANSPARÊNCIA</a>
+			</li>
+			<li class="nav-item">
+				<a style="color:white !important;" class="nav-link" href="sobre.php">QUEM SOMOS</a>
+			</li>
+		</ul>
+    </div>
+  </div>
